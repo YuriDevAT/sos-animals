eda56d1f861a240e13e73c279478744492f3bf74
YuriDevAT <JUndeutsch@gmx.at>
2021-04-22 11:03:23 +0200

    inserted geolocation

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 const Position = () => {
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            setLat(position.coords.latitude);
+            setLong(position.coords.longitude);
+        });
+    }, [lat, long]);
+
+    const handleResponse = (response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Please enable your location!");
+        }
+    }
 
     return (
         <div className="position">
