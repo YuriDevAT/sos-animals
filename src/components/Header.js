58bce9d148afe2562c1491f0123796a5cbfa310a
YuriDevAT <JUndeutsch@gmx.at>
2021-07-13 18:56:13 +0200

    set basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,15 @@ const Header = () => {
 
 
     return (
-        <header className="bg-indigo-300 container h-20 shadow-md">
-            <div className="flex h-full justify-between text-center items-center">
-                <button className="w-20 h-20 bg-green-200" onClick="">B</button>
-                <div className="w-16 h-16 bg-blue-200 rounded-full" >1</div>
-                <div className="w-16 h-16 bg-red-200 rounded-full" >2</div>
-                <div className="w-16 h-16 bg-yellow-200 rounded-full" >3</div>
-                <div className="w-16 h-16 bg-purple-200 rounded-full" >4</div>
+        <header className="bg-blue-600 container h-20 shadow-md fixed max-w-sm">
+            <div className="h-full">
+                <button className="w-20 h-20 bg-yellow-300 float-left" onClick="">B</button>
+                <div className="flex h-full justify-around text-center items-center">
+                    <div className="w-14 h-14 bg-yellow-300 rounded-full" >1</div>
+                    <div className="w-14 h-14 bg-yellow-300 rounded-full" >2</div>
+                    <div className="w-14 h-14 bg-yellow-300 rounded-full" >3</div>
+                    <div className="w-14 h-14 bg-yellow-300 rounded-full" >4</div>
+                </div>
             </div>
             <ul className="nav-list hidden">
                 <li>Start</li>
