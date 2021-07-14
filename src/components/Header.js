b44a9e134e7e4b896523163b94609961ec5e21ca
YuriDevAT <JUndeutsch@gmx.at>
2021-07-14 12:57:14 +0200

    add function to menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React,  { useState } from 'react';
 
 const Header = () => {
-    // const [navbar, setNavbar] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const hide = () => setIsOpen(false);
+    const show = () => setIsOpen(true);
 
-    // const NavbarHandler = (e) => {
-    //     e.preventDefault();
-    //     setNavbar(false);        
-    // }
-
+    const navigation = [
+    { link: '#', text: 'Start' },
+    { link: '#', text: 'About' },
+    { link: '#', text: 'NGO\'s' }
+    ];
 
     return (
         <header className="bg-blue-600 container h-20 shadow-md fixed max-w-sm">
-            <div className="h-full">
-                <button className="w-20 h-20 bg-yellow-300 float-left" onClick="">B</button>
+            <div className="h-full relative">
+                <nav className="relative">
+                    <button className="w-20 h-20 bg-yellow-300 float-left menu-toggle" onClick={toggle}>
+                        <span className={`menu ${isOpen ? 'cross' : 'hamburger'}`}></span>
+                    </button>
+                    <ul className={`menu-links ${isOpen ? 'show' : ''} mt-20 bg-yellow-300 w-1/2 shadow-lg z-20`}>
+                        {navigation.map((nav) => (
+                            <li key={nav.text} className="py-3 border bg-yellow-300 pl-2">
+                                <a href={nav.link} onClick={toggle} onBlur={hide}>{nav.text}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className={`menu-links ${isOpen ? 'show' : ''} mt-20 bg-black opacity-60 h-screen w-full z-10`}></div>
                 <div className="flex h-full justify-around text-center items-center">
                     <div className="w-14 h-14 bg-yellow-300 rounded-full" >1</div>
                     <div className="w-14 h-14 bg-yellow-300 rounded-full" >2</div>
@@ -20,13 +35,7 @@ const Header = () => {
                     <div className="w-14 h-14 bg-yellow-300 rounded-full" >4</div>
                 </div>
             </div>
-            <ul className="nav-list hidden">
-                <li>Start</li>
-                <li>About NGO's</li>
-                <li>About the app</li>
-                <li>AGB</li>
-                <li>Impressum</li>
-            </ul>
+            
         </header>
 
     )
