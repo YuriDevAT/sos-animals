e03304810ada34a0aeb94edd9ec36d03e8d37f3b
YuriDevAT <JUndeutsch@gmx.at>
2021-07-27 16:28:38 +0200

    set app to default

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Formular from './components/Formular';
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 const App = () => {
-  const [step, setStep] = useState(1);
-
-  const prevStep = () => {
-    setStep(step - 1);
-  }
-
-  const nextStep = () => {
-      setStep(step + 1)
-  }
-
-
-
-
 
   return (
         <div className="app">   
           <Header />
           <Formular />
-          <Footer prevStep={prevStep} nextStep={nextStep} />
         </div>
   );
 }
