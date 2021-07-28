bb15c05ec82c818681b1623d382362863d256ab6
Julia Undeutsch <JUndeutsch@gmx.at>
2021-07-28 15:56:26 +0200

    style component

--- src/components/Success.js
+++ src/components/Success.js
@@ -4,9 +4,9 @@ const Success = () => {
     return (
         <div className="success">
             <h1>Successfully reported!</h1>
-            <p>Thank you for working together to save the animals.</p>
+            <p id="success-p">Thank you for working together to save the animals.</p>
             <a href="/">
-            <button type="button">Report another animal</button>
+            <button type="button" id="success-btn">Report another animal</button>
             </a>
         </div>
     )
